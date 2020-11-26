a1d0d5efd342f044345ea40dbe4d5a01a891d828
Gerson Sunyé <sunye@protonmail.com>
2020-11-26 08:20:19 +0100

    Move general options to a common file

---
+++ src/js/revealjs-plugins.js
@@ -0,0 +1,5 @@
+{src: 'mde-languages.min.js', async:true, callback: function() { hljs.initHighlightingOnLoad(); }},
+{ src: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', async:true},
+{ src: 'sketch.js', async:true},
+{ src: 'dazzleSketch.js', async:true} 
+
