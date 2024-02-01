701d56cb870f33cda5990c6a47ff1dd2aafce2be
Gerson Sunyé <sunye@atlanmod.org>
2024-02-01 23:42:43 +0100

    update to reveal 4.5

---
+++ js/revealjs-plugins.js
@@ -0,0 +1,5 @@
+{ src: 'js/mde-languages.min.js', async:true, callback: function() { hljs.initHighlightingOnLoad(); }},
+{ src: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', async:true},
+{ src: 'js/sketch.js', async:true},
+{ src: 'js/dazzleSketch.js', async:true} 
+
